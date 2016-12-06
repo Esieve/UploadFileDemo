edc2ff051a34ab18e8eb8fb7664e746df9f68eb8
codesign-er <772392166@qq.com>
2016-12-06 20:07:07 +0800

    complete service to save infomation into database

---
+++ src/service/FileService.java
@@ -0,0 +1,29 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import entity.File;
+import util.DBUtil;
+
+public class FileService {
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+
+	public void save(File file) throws SQLException {
+		conn = DBUtil.getCONN();
+		String sql = "insert into file(username,filename,filecontent) values (?,?,?)";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, file.getUserName());
+		pstmt.setString(2, file.getFileName());
+		pstmt.setBytes(3, file.getFileContent());
+		pstmt.execute();
+		if (conn != null) {
+			conn.close();
+		}
+		if (pstmt != null) {
+			pstmt.close();
+		}
+	}
+}
