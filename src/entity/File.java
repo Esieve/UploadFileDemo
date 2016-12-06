2fc1ed3e04b9b305b8e70965daf348658e48ca93
codesign-er <772392166@qq.com>
2016-12-06 17:00:42 +0800

    complete entity

--- src/entity/File.java
+++ src/entity/File.java
@@ -1,5 +1,40 @@
 package entity;
 
 public class File {
+	private int id;
+	private String userName;
+	private String fileName;
+	private byte[] fileContent;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public byte[] getFileContent() {
+		return fileContent;
+	}
+
+	public void setFileContent(byte[] fileContent) {
+		this.fileContent = fileContent;
+	}
 }
