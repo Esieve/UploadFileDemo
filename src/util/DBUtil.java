56cd4a4b7d0e47c014423dcf34d6e44332b88a1b
codesign-er <772392166@qq.com>
2016-12-06 17:33:29 +0800

    complete the class DBUtil to implement the connection between java and mysql database

---
+++ src/util/DBUtil.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	private static final String URL = "jdbc:mysql://localhost:3306/uploadfile?useSSL=false";
+	private static final String USER = "root";
+	private static final String PWD = "1011";
+	private static Connection CONN = null;
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			CONN = DriverManager.getConnection(URL, USER, PWD);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static Connection getCONN() {
+		return CONN;
+	}
+}
